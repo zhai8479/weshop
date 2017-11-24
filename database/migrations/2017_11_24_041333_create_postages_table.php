26f6c70d5ae8d18410cfd778834dbdcfb1d36c6a
ZT <zt8479@gmail.com>
2017-11-24 14:25:54 +0800

    完成建表，部分修改

---
+++ database/migrations/2017_11_24_041333_create_postages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('postages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('strategy')->comment('邮费策略，1.包邮 2.满包 3.不包');
+            $table->float('amount')->comment('邮费金额');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('postages');
+    }
+}
