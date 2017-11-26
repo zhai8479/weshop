d8bf5212d74e9c89d5b8fb9e8c1e0a24c2de1bba
ZT <zt8479@gmail.com>
2017-11-26 11:29:47 +0800

    jwt报错信息修改

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,5 +60,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'jwt.auth' => 'Tymon\JWTAuth\Middleware\GetUserFromToken',
         'jwt.refresh' => 'Tymon\JWTAuth\Middleware\RefreshToken',
+        'jwt.api.auth' => \App\Http\Middleware\GetUserFromToken::class,
     ];
 }
